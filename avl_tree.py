014cb59e1c698a9c57d4094afb5644bc5532d524
vovasyukov <vovasyukov@domclick.ru>
2020-10-11 21:35:11 +0300

    avl_tree

--- avl_tree.py
+++ avl_tree.py
@@ -1,93 +1,170 @@
-from binary_search_tree import BinarySearchTree, TreeNode
+# https://blog.coder.si/2014/02/how-to-implement-avl-tree-in-python.html
+
+class AvlNode:
+
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.key)
 
 
-class AVLTreeNode(TreeNode):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.balance_factor = 0
+class AvlTree:
 
+    def __init__(self):
+        self.node = None
+        self.height = -1
+        self.balance = 0
 
-class AVLTree(BinarySearchTree):
-    @staticmethod
-    def create_node(key, value, parent=None):
-        return AVLTreeNode(key, value, parent=parent)
+    def insert(self, key):
+        n = AvlNode(key)
 
-    def _put(self, key, value, current_node):
-        if key < current_node.key:
-            if current_node.has_left_child():
-                self._put(key, value, current_node.left_child)
-            else:
-                current_node.left_child = self.create_node(key, value, current_node)
-                self.update_balance(current_node.left_child)
-        elif key > current_node.key:
-            if current_node.has_right_child():
-                self._put(key, value, current_node.right_child)
-            else:
-                current_node.right_child = self.create_node(key, value, current_node)
-                self.update_balance(current_node.right_child)
+        if not self.node:
+            self.node = n
+            self.node.left = AvlTree()
+            self.node.right = AvlTree()
+
+        elif key < self.node.key:
+            self.node.left.insert(key)
+
+        elif key > self.node.key:
+            self.node.right.insert(key)
+
+        self.rebalance()
+
+    def rebalance(self):
+        self.update_heights(recursive=False)
+        self.update_balances(False)
+
+        while self.balance < -1 or self.balance > 1:
+            if self.balance > 1:
+                if self.node.left.balance < 0:
+                    self.node.left.rotate_left()
+                    self.update_heights()
+                    self.update_balances()
+
+                self.rotate_right()
+                self.update_heights()
+                self.update_balances()
+
+            if self.balance < -1:
+                if self.node.right.balance > 0:
+                    self.node.right.rotate_right()  # we're in case III
+                    self.update_heights()
+                    self.update_balances()
+
+                self.rotate_left()
+                self.update_heights()
+                self.update_balances()
+
+    def update_heights(self, recursive=True):
+        if self.node:
+            if recursive:
+                if self.node.left:
+                    self.node.left.update_heights()
+                if self.node.right:
+                    self.node.right.update_heights()
+
+            self.height = 1 + max(self.node.left.height, self.node.right.height)
         else:
-            current_node.value = value
+            self.height = -1
 
-    def update_balance(self, node):
-        if node.balance_factor > 1 or node.balance_factor < -1:
-            self.rebalance(node)
-            return
-        if node.parent is not None:
-            if node.is_left_child():
-                node.parent.balance_factor += 1
-            elif node.is_right_child():
-                node.parent.balance_factor -= 1
+    def update_balances(self, recursive=True):
+        if self.node:
+            if recursive:
+                if self.node.left:
+                    self.node.left.update_balances()
+                if self.node.right:
+                    self.node.right.update_balances()
 
-            if node.parent.balance_factor != 0:
-                self.update_balance(node.parent)
-
-    def rebalance(self, node):
-        if node.balance_factor < 0:
-            if node.right_child.balance_factor > 0:
-                self.rotate_right(node.right_child)
-            self.rotate_left(node)
-        elif node.balance_factor > 0:
-            if node.left_child.balance_factor < 0:
-                self.rotate_left(node.left_child)
-            self.rotate_right(node)
-
-    def rotate_left(self, old_root):
-        new_root = old_root.right_child
-        old_root.right_child = new_root.left_child
-
-        if new_root.has_left_child():
-            new_root.left_child.parent = old_root
-
-        new_root.parent = old_root.parent
-        if old_root.is_root():
-            self.root = new_root
+            self.balance = self.node.left.height - self.node.right.height
         else:
-            if old_root.is_left_child():
-                old_root.parent.left_child = new_root
-            else:
-                old_root.parent.right_child = new_root
+            self.balance = 0
 
-        new_root.left_child = old_root
-        old_root.parent = new_root
-        old_root.balance_factor = old_root.balance_factor + 1 - min(new_root.balance_factor, 0)
-        new_root.balance_factor = new_root.balance_factor + 1 + max(old_root.balance_factor, 0)
+    def rotate_right(self):
+        new_root = self.node.left.node
+        new_left_sub = new_root.right.node
+        old_root = self.node
+
+        self.node = new_root
+        old_root.left.node = new_left_sub
+        new_root.right.node = old_root
+
+    def rotate_left(self):
+        new_root = self.node.right.node
+        new_left_sub = new_root.left.node
+        old_root = self.node
+
+        self.node = new_root
+        old_root.right.node = new_left_sub
+        new_root.left.node = old_root
+
+    def delete(self, key):
+        if self.node is not None:
+            if self.node.key == key:
+                if not self.node.left.node and not self.node.right.node:
+                    self.node = None
+
+                elif not self.node.left.node:
+                    self.node = self.node.right.node
+
+                elif not self.node.right.node:
+                    self.node = self.node.left.node
+                else:
+                    successor = self.node.right.node
+                    while successor and successor.left.node:
+                        successor = successor.left.node
+
+                    if successor:
+                        self.node.key = successor.key
+                        self.node.right.delete(successor.key)
+
+            elif key < self.node.key:
+                self.node.left.delete(key)
+
+            elif key > self.node.key:
+                self.node.right.delete(key)
+
+            self.rebalance()
+
+    def inorder_traverse(self):
+        result = []
+
+        if not self.node:
+            return result
+
+        result.extend(self.node.left.inorder_traverse())
+        result.append(self.node.key)
+        result.extend(self.node.right.inorder_traverse())
+
+        return result
+
+    def display(self, node=None, level=0):
+        if not node:
+            node = self.node
+
+        if node.right.node:
+            self.display(node.right.node, level + 1)
+            print('\t' * level, '    /')
+
+        print('\t' * level, node)
+
+        if node.left.node:
+            print('\t' * level, '    \\')
+            self.display(node.left.node, level + 1)
 
 
-if __name__ == '__main__':
-    tree = AVLTree()
-    tree[30] = "red"
-    tree[4] = "blue"
-    tree[6] = "yellow"
-    tree[2] = "at"
-    tree[8] = 'df'
-    tree.print_yourself()
-    tree[45] = "as"
-    tree[32] = "asd"
-    tree[31] = "aaa1"
-    tree[33] = "aaa2"
-    tree[34] = "aaa3"
-    tree[3] = "aaa3"
-    tree[48] = 'as'
-    tree[47] = 'asdf'
+if __name__ == "__main__":
+    tree = AvlTree()
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-    tree.print_yourself()
+    for key in data:
+        tree.insert(key)
+
+    for key in [4, 3]:
+        tree.delete(key)
+
+    print(tree.inorder_traverse())
+    tree.display()
