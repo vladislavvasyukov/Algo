0afabb47826acc6fc154da504883aa5f18f129b2
Vladislav Vasiukov <vladislav.vasiukov@exness.com>
2023-12-25 20:40:37 +0200

    fix

---
+++ my_await.py
@@ -0,0 +1,37 @@
+import asyncio
+import functools
+import time
+from typing import Callable, Any
+
+from aiohttp import ClientSession
+
+
+async def delay(seconds):
+    print("before delay", seconds)
+    await asyncio.sleep(seconds)
+    print("after delay", seconds)
+    return seconds
+
+
+def async_timed():
+    def wrapper(func: Callable) -> Callable:
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f'starting {func} with args {args} {kwargs}')
+            start = time.time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time.time()
+                total = end - start
+                print(f'finished {func} in {total:.4f} second(s)')
+
+        return wrapped
+
+    return wrapper
+
+@async_timed()
+async def fetch_status(session: ClientSession, url: str, delay: int = 0) -> int:
+    await asyncio.sleep(delay)
+    async with session.get(url) as result:
+        return result.status
